0baed0507b6bb4d47d9953e7f662709026dab7b9
panickylemon <panickylemon>
2017-01-20 15:55:15 +0300

    tab

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-$(document ).ready(function() {
+$(document).ready(function () {
     $(".b-search__expand")
-    .focus(function(e){
-        $("#search-button").addClass("b-search__button--focus");
-        $(this).attr("placeholder", "Введите запрос");
-    })
-    .blur(function(e){
-        setTimeout(function(){
-            $("#search-button").removeClass("b-search__button--focus");
-        },300);
-        $(this).attr("placeholder", "Поиск");
-    });
+        .focus(function (e) {
+            $("#search-button").addClass("b-search__button--focus");
+            $(this).attr("placeholder", "Введите запрос");
+        })
+        .blur(function (e) {
+            setTimeout(function () {
+                $("#search-button").removeClass("b-search__button--focus");
+            }, 300);
+            $(this).attr("placeholder", "Поиск");
+        });
 
 
-    $(".header__menu-item").each(function(){
-        if ($(this).children('ul').length != 0){
+    $(".header__menu-item").each(function () {
+        if ($(this).children('ul').length != 0) {
             $(this).addClass("menu-item__has-child");
         }
     });
 
-    $(".header__menu-burger").click(function() {
+    $(".header__menu-burger").click(function () {
         $(".header__menu").slideToggle("slow");
-        if( $('.icon-bar').is(':visible') ) {
+        if ($('.icon-bar').is(':visible')) {
             $(".icon-bar").hide();
         } else {
             $(".icon-bar").delay(500).fadeIn(100);
         }
-        if( $('.icon-close').is(':visible') ) {
+        if ($('.icon-close').is(':visible')) {
             $(".icon-close").delay(300).fadeOut(100);
         } else {
             $(".icon-close").show();
@@ -35,27 +35,28 @@ $(document ).ready(function() {
 
     $('.owl-carousel').owlCarousel({
         items: 1,
-        loop:true, //Зацикливаем слайдер
-        nav:false, //Отключена навигация
+        loop: true, //Зацикливаем слайдер
+        nav: false //Отключена навигация
         // autoplay:true, //Автозапуск слайдера
         // smartSpeed:1000, //Время движения слайда
         // autoplayTimeout:2000, //Время смены слайда
-        responsive:{ //Адаптация в зависимости от разрешения экрана
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
+    });
+
+
+    // Tab
+    $('#productsTab').easyResponsiveTabs({
+        type: 'default', //Types: default, vertical, accordion
+        width: 'auto', //auto or any width like 600px
+        fit: true, // 100% fit in a container
+        tabidentify: 'hor_1', // The tab groups identifier
+        activate: function (event) { // Callback function if tab is switched
+            var $tab = $(this);
+            var $info = $('#nested-tabInfo');
+            var $name = $('span', $info);
+            $name.text($tab.text());
+            $info.show();
         }
     });
 
-    // $('#demoTab').easyResponsiveTabs({ tabidentify: 'vert' });
-    // $('#myTab').tabCollapse({
-    //     tabsClass: 'hidden-sm hidden-xs',
-    //     accordionClass: 'visible-sm visible-xs'
-    // });
+
 });
