30c5f9dd066c37d1fda29cfde2c4613574f4fddb
panickylemon <panickylemon>
2017-01-20 17:02:21 +0300

    tab

--- js/main.js
+++ js/main.js
@@ -49,6 +49,7 @@ $(document).ready(function () {
         width: 'auto', //auto or any width like 600px
         fit: true, // 100% fit in a container
         tabidentify: 'hor_1', // The tab groups identifier
+
         activate: function (event) { // Callback function if tab is switched
             var $tab = $(this);
             var $info = $('#nested-tabInfo');
